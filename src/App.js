d61f39a2426b40ab8226b7c43eeff5aa34907cb0
Diego Vielma <diego@ugami.com>
2023-06-05 18:18:13 -0400

    Favicon and Title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import './index.css'
 
 const App = () => {
+  useEffect(() => {
+    document.title = 'Sales System Organizer';
+  }, []);
   const [files, setFiles] = useState();
   const [ loginData ] = useState({
     user: '',
